1396230224f3428e53c337d0caa2fc82ba9640c3
Miro Nurmela <miro.nurmela@futurice.com>
2019-11-09 07:59:29 +0200

    Reduce the amount of items in the sorting test

--- src/Sorting/SortingTests.js
+++ src/Sorting/SortingTests.js
@@ -2,7 +2,7 @@ import { generateRandomArray } from "./SortingUtils";
 import { testSorting } from "../TestFrameWork/testFunctions";
 
 export const testSortingFunction = (sortingFunction, functionName) => {
-  const arrayToSort = generateRandomArray(1000, 0, 1000);
+  const arrayToSort = generateRandomArray(20, 0, 1000);
   const sortedArray = sortingFunction(arrayToSort);
   return testSorting(sortedArray, `Testing ${functionName}`);
 };
